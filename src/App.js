486c992246595b3be66f30dfcd56e13c3c90d4de
Raichu15678 <matefekete355@gmail.com>
2025-10-02 14:13:15 +0200

    a github egy kalap szar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ function App() {
   return (
     <div className="App">
       <h1>Pályorientáció</h1>
+      <p>Ha pályaváltás, vagyis új, az eddigitől eltérő foglalkozás választása előtt áll, érdemes 
+tisztáznia, hogy milyen képességekkel rendelkezik. A képességek erősen befolyásolják, hogy 
+pályáján, foglalkozásának gyakorlásánál mennyire sikeres. Az alábbi kérdőív ebben jelenthet 
+segítséget: kitöltésével tájékozódhat a képességeiről, melyek jelentik az erősségeit és melyek 
+azok, amelyekben gyengébb. </p>
     </div>
   );
 }
